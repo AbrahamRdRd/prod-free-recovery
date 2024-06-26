93d6058b9bf7b9b5330fc1703d68d35aef901075
AbrahamRdRd <75872944+AbrahamRdRd@users.noreply.github.com>
2024-06-25 19:42:53 -0600

    Prueba con todos los componentes
    Prueba con todos los componentes

---
+++ dist/assets/ErrorPage-legacy-B3jRSr-4.js
@@ -0,0 +1,1 @@
+System.register(["./_plugin-vue_export-helper-legacy-DgAO6S8O.js","./index-legacy-CW1NlSFU.js"],(function(e,r){"use strict";var t,n,u;return{setters:[e=>{t=e._},e=>{n=e.o,u=e.c}],execute:function(){e("default",t({},[["render",function(e,r){return n(),u("h1",null,"Error")}]]))}}}));
---
+++ dist/assets/user-legacy-XgMERCGC.js
@@ -0,0 +1,1 @@
+System.register(["./https-legacy-DyGVZRWv.js"],(function(e,s){"use strict";var t;return{setters:[e=>{t=e.h}],execute:function(){e("g",(e=>t.get(`/api/users/${e}`))),e("c",(e=>t.post("/api/users",e))),e("u",((e,s)=>t.post(`/api/users/${e}?_method=PUT`,s)))}}}));
