ae97d67aeb6b226d8b8b3e70bf187ea4c6aa66fa
AbrahamRdRd <75872944+AbrahamRdRd@users.noreply.github.com>
2024-07-08 11:31:14 -0600

    Commit completo para prod
    Commit completo para prod

---
+++ dist/assets/user-Dq2FRYsz.js
@@ -0,0 +1,1 @@
+import{h as t}from"./https-D9ix251n.js";const r=s=>t.get("/api/users/".concat(s)),o=s=>t.post("/api/users",s),p=(s,a)=>t.post("/api/users/".concat(s,"?_method=PUT"),a),u=(s,a)=>t.post("/api/users/".concat(s,"/avatar?_method=PATCH"),a);export{p as a,o as c,r as g,u};
