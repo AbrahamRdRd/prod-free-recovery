3f6aa4a6c6da26eaf491fb6a05c0aa5ad188ae2e
AbrahamRdRd <75872944+AbrahamRdRd@users.noreply.github.com>
2024-07-18 07:40:42 -0600

    Reparando bug en registro
    Reparando bug en registro

---
+++ dist/assets/auth-legacy-DMHNpBD_.js
@@ -0,0 +1,1 @@
+System.register(["./https-legacy-B7tzuJSA.js"],(function(t,s){"use strict";var e;return{setters:[t=>{e=t.h}],execute:function(){t("a",(t=>e.post("/api/auth/token",t))),t("f",(t=>e.post("/api/auth/forgot-password",t))),t("r",(t=>e.post("/api/auth/reset-password",t)))}}}));
