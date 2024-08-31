6f2d4d299f9324a5d828a395d987f93e670e9444
AbrahamRdRd <75872944+AbrahamRdRd@users.noreply.github.com>
2024-08-31 12:02:57 -0600

    Delete dist directory
    deleting

--- dist/assets/countries-legacy-BgIn3Ejb.js
+++
@@ -1,1 +0,0 @@
-System.register(["./https-legacy-CY25jbHb.js"],(function(t,e){"use strict";var s;return{setters:[t=>{s=t.h}],execute:function(){t("g",(()=>s.get("/api/countries"))),t("c",(()=>s.get("/api/country-codes")))}}}));
