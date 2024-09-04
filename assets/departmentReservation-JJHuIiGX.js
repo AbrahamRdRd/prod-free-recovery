cc363852727a79dd261972950c03969f842e7063
AbrahamRdRd <75872944+AbrahamRdRd@users.noreply.github.com>
2024-09-04 13:39:28 -0600

    Version Beta!
    Version Beta!

---
+++ assets/departmentReservation-JJHuIiGX.js
@@ -0,0 +1,1 @@
+import{h as e}from"./https-CNct6Qw_.js";const a=(t,s)=>e.post("/api/departments/".concat(t,"/reservations"),s),o=t=>e.get("/api/users/".concat(t,"/reservations"));export{a as c,o as g};
